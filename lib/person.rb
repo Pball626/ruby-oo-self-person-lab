3aebbbac0d1c51c431b96f33c761d1644913c1ce
github username <github email address>
2021-02-25 15:24:42 -0600

    commit tree 911d09b049d20c7c13ba2356360e07d57d8d8a18

--- lib/person.rb
+++ lib/person.rb
@@ -49,28 +49,27 @@ binding.pry
     end
     binding.pry
 
-    def get_paid( )
-        puts "all about the benjamins"
-    end
+    # def get_paid( )
+    #     puts "all about the benjamins"
+    # end
     
-    def take_bath #increment hygeine points by 4
-        return "Rub-a-dub just relaxing in the tub".
+    # def take_bath #increment hygeine points by 4
+    #     return "Rub-a-dub just relaxing in the tub".
     
-    def work_out #increase happiness by 2
-                 #decrease hygeine by 3    
-        return "another one bites the dust".
+    # def work_out #increase happiness by 2
+    #              #decrease hygeine by 3    
+    #     return "another one bites the dust".
     
-    def call_friend(friend)
-        returns "Hi {@friend}! It's Stella. How are you"
-    end
+    # def call_friend(friend)
+    #     returns "Hi {@friend}! It's Stella. How are you"
+    # end
     
-    def start_conversation(person, topic)
-        if topic == politics #decrease sadness
-            return "blah blah partisan blah lobbyist"
-        elseif topic == weather #increase happiness
-            return "blah blah sun blah rain" 
-        else #happiness points dont change
-            return "blah blah blah blah blah"
+    # def start_conversation(person, topic)
+    #     if topic == politics #decrease sadness
+    #         return "blah blah partisan blah lobbyist"
+    #     elseif topic == weather #increase happiness
+    #         return "blah blah sun blah rain" 
+    #     else #happiness points dont change
+    #         return "blah blah blah blah blah"
     
 end
-
